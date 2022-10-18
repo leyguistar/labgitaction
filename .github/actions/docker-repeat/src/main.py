a96f7ec3fa0a57c19080e56e8edf21df903e52d4
DavidSanchez <luigisperstar7@gmail.com>
2022-10-17 21:20:36 -0400

    arreglado algo en el python

--- .github/actions/docker-repeat/src/main.py
+++ .github/actions/docker-repeat/src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 #solo para invocar otro push
-text = os.environ("INPUT_INPUT_TEXT")
-n = os.environ("INPUT_NUM_OF_REPEATS")
+text = os.environ["INPUT_INPUT_TEXT"]
+n = os.environ["INPUT_NUM_OF_REPEATS"]
 output = text*n
 print(f'set-output name=output_text::{output}')
