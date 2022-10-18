7fb6ec72e42d99aafe84e0d561545fc1c46e6891
DavidSanchez <luigisperstar7@gmail.com>
2022-10-17 21:21:26 -0400

    XD

--- .github/actions/docker-repeat/src/main.py
+++ .github/actions/docker-repeat/src/main.py
@@ -2,6 +2,6 @@
 import os
 #solo para invocar otro push
 text = os.environ["INPUT_INPUT_TEXT"]
-n = os.environ["INPUT_NUM_OF_REPEATS"]
+n = int(os.environ["INPUT_NUM_OF_REPEATS"])
 output = text*n
 print(f'set-output name=output_text::{output}')
